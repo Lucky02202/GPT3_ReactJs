bf23197829003722d6206245364488677bdc480f
Laxmikant Patil <laxmikant.patil02202@gmail.com>
2023-07-01 18:11:29 +0530

    added footer , made the whole website responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -24,14 +24,18 @@ const Navbar = () => {
         <div className="gpt3__navbar-links_logo">
           <img src={logo} alt="logo" />
         </div>
+
         <div className="gpt3__navbar-links_container">
           <Menu />
         </div>
+
       </div>
+
       <div className="gpt3__navbar-sign">
         <p>Sign in</p>
         <button type="button">Sign up</button>
       </div>
+
       <div className="gpt3__navbar-menu">
         {toggleMenu ? (
           <RiCloseLine
